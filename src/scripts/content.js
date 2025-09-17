e6f685a79d346047aacf9aec59288a541ef5e344
faiz <1919532973@qq.com>
2025-09-17 18:32:26 +0800

    脚本上传

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -6,7 +6,27 @@ script.src = chrome.runtime.getURL("assets/inject.js");
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.action === "popupOpened") {
-    // sendResponse(sessionStorage.getItem("c-Shopify-info"));
+    try {
+      document
+      .querySelectorAll("form.elementor-form .elementor-field")
+      .forEach((i) => {
+        let obj = {
+          text: `test: ${i.placeholder}`,
+          tel: "15297614000",
+          email: "test@ehaitech.com",
+          textarea: "This is test message",
+        };
+        i.value = obj[i.type];
+        if (i.name == "zendkee_captcha") {
+          i.value = i
+            .closest(".elementor-field-group")
+            .querySelector(".captcha_code").textContent;
+        }
+      });
+    } catch (error) {
+      console.log('@@@ 表单填充异常', error);
+    }
+    
   } else if (message.action === "judgePage") {
     // sendResponse(sessionStorage.getItem("c-Shopify-info"));
   }
